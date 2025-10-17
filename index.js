ee0995d695fd1e85ad36a66599303d3bc8d9a937
dhanush523 <dhanushshankar74@gmail.com>
2025-10-17 15:17:36 +0530

    Your update message

--- index.js
+++ index.js
@@ -4,4 +4,22 @@ function toggleMenu() {
     }
 
 
+ function openGmail() {
+    const email = "dhanushshankar74@gmail.com";
+    const subject = "Inquiry about Mini Projects ";
+const body = "Hello MiniCode Studio, I am interested in purchasing a project . Please provide more details.";
+
     
+    const isMobile = /Android|iPhone|iPad|iPod/i.test(navigator.userAgent);
+
+    if (isMobile) {
+      // 📱 Mobile → open Gmail app (mailto:)
+      window.location.href = `mailto:${email}?subject=${subject}&body=${body}`;
+    } else {
+      // 💻 Desktop → open Gmail web
+      window.open(
+        `https://mail.google.com/mail/?view=cm&fs=1&to=${email}&su=${subject}&body=${body}`,
+        "_blank"
+      );
+    }
+  }   
